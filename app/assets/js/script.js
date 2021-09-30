6d20ad35d42545a3e581c31dda27b0f3fa9da41b
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2021-09-30 10:35:56 +0600

    Add files via upload

---
+++ app/assets/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // code should be execute here
+
+});
